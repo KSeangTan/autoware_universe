e07bdd727c83e4a257014c47b4c63765cb2d360b
Kok Seang Tan <kseangtan@gmail.com>
2025-05-28 14:17:45 +0900

    Added

--- perception/autoware_lidar_centerpoint/include/autoware/lidar_centerpoint/centerpoint_config.hpp
+++ perception/autoware_lidar_centerpoint/include/autoware/lidar_centerpoint/centerpoint_config.hpp
@@ -17,6 +17,7 @@
 
 #include <cstddef>
 #include <vector>
+#include <string>
 
 namespace autoware::lidar_centerpoint
 {
@@ -131,6 +132,8 @@ public:
   float offset_z_ = range_min_z_ + voxel_size_z_ / 2;
   std::size_t down_grid_size_x_ = grid_size_x_ / downsample_factor_;
   std::size_t down_grid_size_y_ = grid_size_y_ / downsample_factor_;
+
+  // logger name
   std::string logger_name_{"lidar_centerpoint"};
 };
 
